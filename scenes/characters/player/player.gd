4a5cff431b33cf48b90fd9ace538cb374a7a9566
Felix <fedecraft3312@gmail.com>
2022-05-19 16:32:21 -0300

    Animation changes when the player is moving, and keys for opening and closing the inventory

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,9 +5,12 @@ extends KinematicBody2D
 # [] Exp system
 # [] Attributes(vitality, endurance, agility, intelligence, etc)
 
+onready var anim_player:AnimationPlayer = $AnimationPlayer
 onready var inv_mng:InventoryManager = $InventoryManager
+onready var inv_timer:Timer = $InventoryVisibleStateTimer
 
-export (int) var speed = 200
+export(float) var inv_delay = 0.3
+export(int) var speed = 200
 
 var input_handler = preload("res://godot-libs/inputs/" +
 	"input_handler.gd").new({ "disable_wasd": false,
@@ -18,7 +21,7 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.inv_mng.set_info({ "debug": true })
+	self.inv_mng.set_info({ "debug": false })
 	
 	# The inventory gets added to the inventory_manager and is set to
 	# inv_ui variable
@@ -27,7 +30,7 @@ func _ready():
 	
 	# Change ui visibility to false, so it doesn't start with the inventory
 	# open
-	self.inv_ui.set_info({ "debug": true, "visible": true, })
+	self.inv_ui.set_info({ "debug": false, "visible": false, })
 	
 	# Get a reference to the inventory script that contains the items
 	self.inv = self.inv_ui.cells_manager.inventory
@@ -44,7 +47,24 @@ func _ready():
 # Physics process tries to perform operations at a constant framerate
 func _physics_process(delta):
 	get_input()
+	
+	# Check if the user opened or closed the inventory
+	update_inventory_visible_state()
+	
 	velocity = move_and_slide(velocity)
+
+
+func attack():
+	var anim_list = anim_player.get_animation_list()
+	
+	for anim_name in anim_list:
+		match(anim_player.current_animation):
+			"WalkDown":
+				anim_player.play("AttackDown")
+			"WalkUp":
+				anim_player.play("AttackUp")
+			"_":
+				continue
 
 
 # Change velocity depending on the player input, it's normalized, so that
@@ -53,12 +73,16 @@ func get_input():
 	velocity = Vector2()
 	if(input_handler.right()):
 		velocity.x += 1
+		anim_player.current_animation = "WalkRight"
 	if(input_handler.left()):
 		velocity.x -= 1
+		anim_player.current_animation = "WalkLeft"
 	if(input_handler.up()):
 		velocity.y -= 1
+		anim_player.current_animation = "WalkUp"
 	if(input_handler.down()):
 		velocity.y += 1
+		anim_player.current_animation = "WalkDown"
 	
 	# With normalize we prevent the player from having
 	# extra speed while moving diagonally
@@ -77,3 +101,26 @@ func set_inv_ui(value) -> void:
 	inv_ui = value
 func get_inv_ui():
 	return inv_ui
+
+
+# Open or close the inventory
+func update_inventory_visible_state():
+	# It's too fast!, we need a cooldown.
+	
+	var is_inv_key_pressed = false
+	
+	if(Input.is_physical_key_pressed(KEY_I)):
+		is_inv_key_pressed = true
+		# The first argument is the device connected)?
+		# JOY_L3 = 8 (Left stick click)
+	elif(Input.is_joy_button_pressed(0, 8)):
+		is_inv_key_pressed = true
+	
+	# The player pressed the key to open or close inv
+	if(is_inv_key_pressed && inv_timer.is_stopped()):
+		# If the inventory is open close it, otherwise open it
+		self.inv_ui.visible = !self.inv_ui.visible
+		
+		# Start the timer(if not it would open and close
+		# every frame the keys are pressed)
+		inv_timer.start(inv_delay)
