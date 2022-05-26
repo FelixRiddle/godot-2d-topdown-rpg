5217b964ab000f4ab01e364c1f578675f5df9c8a
Felix <fedecraft3312@gmail.com>
2022-05-26 16:57:43 -0300

    Added some stuff

--- scenes/objects/object_data.gd
+++ scenes/objects/object_data.gd
@@ -1,14 +1,28 @@
 class_name ObjectData
 extends Node
 
-static func get_tree_sprite_by_id(id=0):
-	var trees:Array = [
+# Tree sprites
+static func get_tree_sprites() -> Array:
+	return [
 		"res://art/godot-2d-topdown-rpg-sprites/plants/trees/" + \
 				"normal_tree.png",
 		"res://art/godot-2d-topdown-rpg-sprites/plants/trees/" + \
 				"normal_tree_2.png",
 		"res://art/godot-2d-topdown-rpg-sprites/plants/trees/" + \
-				"tree_no_leaves.png"
+				"tree_no_leaves.png",
 	]
-	
+
+static func get_tree_sprite_by_id(id=0):
+	var trees:Array = get_tree_sprites()
 	return trees[id]
+
+# Bush sprites
+static func get_bush_sprites() -> Array:
+	return [
+		"res://art/godot-2d-topdown-rpg-sprites/plants/bushes/bush1.png",
+		"res://art/godot-2d-topdown-rpg-sprites/plants/bushes/bush2.png",
+	];
+
+static func get_bush_sprite_by_id(id=0):
+	var bushes:Array = get_bush_sprites()
+	return bushes[id]
--- scenes/objects/plants/bushes/bush/bush.gd
+++ scenes/objects/plants/bushes/bush/bush.gd
@@ -5,7 +5,7 @@ var ObjectData = preload("res://scenes/objects/object_data.gd")
 
 export(float) var opacity_change:float = 0.4 setget set_opacity_change, \
 		get_opacity_change
-export(int) var tree_id = 0
+export(int) var bush_id = 0
 
 # Handle object information, like changing the opacity when the player
 # is in the area of the object
@@ -14,7 +14,7 @@ var obj = preload("res://godot-libs/libs/objects/object.gd")
 func _ready():
 	var sprite:Sprite = $Sprite
 	
-	sprite.texture = load(ObjectData.get_tree_sprite_by_id(tree_id))
+	sprite.texture = load(ObjectData.get_bush_sprite_by_id(bush_id))
 	
 	# Create the object and pass a reference of this node
 	obj = obj.new(self)
